c6e915af7839b38bbd5ddf53bb389438b8f12ddf
qinterfly <qinterfly@gmail.com>
2020-12-22 14:09:42 +0300

    v0.1.2. User's selection interface modified. Project reorganized

---
+++ project/src/gui/LMSModel.Interface.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using OpenTK;
+using OpenTK.Graphics.OpenGL;
+
+namespace ResponseAnalyzer
+{
+
+    public partial class LMSModel
+    {
+
+        // Check if it's all the data is correct
+        public bool isCongruent()
+        {
+            return componentNames_ != null && glControl_ != null;
+        }
+
+        public void setControl(OpenTK.GLControl glControl)
+        {
+            glControl_ = glControl;
+            initializeGL();
+        }
+
+        public void setScale(float scale)
+        {
+            modelScale_ *= Matrix4.CreateScale(scale);
+        }
+
+        public void setTranslation(float dX, float dY)
+        {
+            location_[0] += dX;
+            location_[1] += dY;
+            modelTranslation_ = Matrix4.CreateTranslation(location_);
+        }
+
+        public void setRotationXY(float dRotX, float dRotY)
+        {
+            modelRotation_ *= Matrix4.CreateRotationX(dRotX)
+                            * Matrix4.CreateRotationY(dRotY);
+        }
+
+        public void setRotationAxis(Vector3 axis, float dRot)
+        {
+            modelRotation_ *= Matrix4.CreateFromAxisAngle(axis, dRot);
+        } 
+        
+        public void setView(Views view)
+        {
+            location_ = Vector3.Zero;
+            modelTranslation_ = Matrix4.Identity;
+            modelRotation_ = Matrix4.Identity;
+            switch (view)
+            {
+                case Views.FRONT:
+                    setRotationXY(0.0f, 0.0f);
+                    break;
+                case Views.BACK:
+                    setRotationAxis(Vector3.UnitX, MathHelper.TwoPi);
+                    break;
+                case Views.UP:
+                    setRotationAxis(Vector3.UnitX, MathHelper.PiOver2);
+                    break;
+                case Views.DOWN:
+                    setRotationAxis(Vector3.UnitX, -MathHelper.PiOver2);
+                    break;
+                case Views.LEFT:
+                    setRotationAxis(Vector3.UnitY, MathHelper.PiOver2);
+                    break;
+                case Views.RIGHT:
+                    setRotationAxis(Vector3.UnitY, -MathHelper.PiOver2);
+                    break;
+            }
+        }
+
+        public List<string> getSelection()
+        {
+            List<string> selected = new List<string>();
+            foreach (string component in selection_.Keys)
+            {
+                string[] nearestComponentNodes = (string[])componentSet_.nodeNames[component];
+                List<uint> selectedNodes = selection_[component];
+                foreach (int iNode in selectedNodes)
+                {
+                    string nearestNode = nearestComponentNodes[iNode];
+                    selected.Add(component + ":" + nearestNode);
+                }
+            }
+            return selected;
+        }
+
+        public void clearSelection()
+        {
+            selection_.Clear();
+        }
+
+        public void select(int mouseX, int mouseY, bool isNewSelection)
+        {
+            Matrix4 modelView = modelRotation_ * modelScale_ * modelTranslation_ * view_;
+            Point mouseLocation = new Point(mouseX, mouseY);
+            float minDist = float.MaxValue;
+            float dist = 0.0f;
+            Vector3 tempVector = Vector3.Zero;
+            string minComponent = string.Empty;
+            uint minNode = 0;
+            // Finding the nearest model node to the picked one
+            foreach (string component in componentNames_)
+            {
+                float[] vertices = (float[]) componentSet_.vertices[component];
+                int lenVertices = vertices.Length;
+                for (int i = 0; i != lenVertices; i = i + 3)
+                {
+                    tempVector.X = vertices[i    ];
+                    tempVector.Y = vertices[i + 1];
+                    tempVector.Z = vertices[i + 2];
+                    dist = ObjectPicker.DistanceFromPoint(mouseLocation, tempVector, modelView, projection_);
+                    if (dist < minDist)
+                    {
+                        minDist = dist;
+                        minComponent = component;
+                        minNode = (uint) i / 3;
+                    }
+                }
+            }
+            addToSelection(minComponent, minNode, isNewSelection);
+        }
+
+        public void select(string component, string nodeName, bool isNewSelection)
+        {
+            if (!componentSet_.mapNodeNames.ContainsKey(component)
+                || !componentSet_.mapNodeNames[component].ContainsKey(nodeName))
+                return;
+            uint iSelectedNode = componentSet_.mapNodeNames[component][nodeName];
+            addToSelection(component, iSelectedNode, isNewSelection);
+        }
+
+        private void addToSelection(string component, uint iNode, bool isNewSelection)
+        {
+            if (isNewSelection)
+                selection_.Clear();
+            List<uint> indSel;
+            // Adding to the selection set
+            if (selection_.ContainsKey(component))
+            {
+                indSel = selection_[component];
+                // Deselect if it has been already selected
+                if (indSel.Remove(iNode))
+                    return;
+            }
+            else
+            {
+                indSel = new List<uint>();
+                selection_.Add(component, indSel);
+            }
+            indSel.Add(iNode);
+        }
+
+        public void removeSeletion(string component, string node)
+        {
+            if (selection_.ContainsKey(component) && componentSet_.mapNodeNames[component].ContainsKey(node))
+                selection_[component].Remove(componentSet_.mapNodeNames[component][node]);
+        }
+
+        // Control
+        private OpenTK.GLControl glControl_ = null;
+        // Selection
+        private Dictionary<string, List<uint>> selection_;
+    }
+}
---
+++ project/src/gui/MainWindow.cs
@@ -0,0 +1,269 @@
+﻿using System;
+using System.IO;
+using OpenTK;
+using OpenTK.Input;
+using System.Windows.Forms;
+using OpenTK.Graphics.OpenGL;
+
+namespace ResponseAnalyzer
+{
+    public partial class ResponseAnalyzer : Form
+    {
+        public ResponseAnalyzer()
+        {
+            InitializeComponent();
+            comboBoxSelection.SelectedIndex = 0;
+            modelRenderer_ = new LMSModel();
+            lastMousePosition_ = new int[2] { 0, 0 };
+        }
+
+        // Opening project
+        private void buttonOpenProject_Click(object sender, EventArgs e)
+        {
+            clearStatus();
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "LMS Test.Lab 15A Project (*.lms)|*.lms";
+            openFileDialog.FilterIndex = 1;
+            openFileDialog.RestoreDirectory = true;
+            DialogResult dialogResult = openFileDialog.ShowDialog();
+            if (dialogResult == DialogResult.OK)
+            {
+                string filePath = openFileDialog.FileName;
+                project = new LMSProject(filePath);
+                clearProjectControls();
+                if (!project.isProjectOpened()) {
+                    textBoxProjectPath.Text = filePath;
+                    setStatus("The project was successfully opened");
+                }
+                modelRenderer_.setGeometry(project.geometry_);
+            }
+            else if (dialogResult != DialogResult.Cancel)
+            {
+                setStatus("An error occured while choosing a project file");
+                return;
+            }
+            else
+            {
+                return;
+            }
+        }
+
+        // Set the status label
+        private void setStatus(string status)
+        {
+            statusStripLabel.Text = status;
+        }
+
+        // Clear the status label
+        private void clearStatus()
+        {
+            statusStripLabel.Text = null;
+        }
+
+        // Clear project controls
+        private void clearProjectControls()
+        {
+            textBoxProjectPath.Clear();
+        }
+
+
+        private void buttonAddSelection_Click(object sender, EventArgs e)
+        {
+            var selection = modelRenderer_.getSelection();
+            int nSelection = selection.Count;
+            if (nSelection == 0)
+                return;
+            if (nSelection > 1)
+            {
+                TreeNode line = treeSelection.Nodes.Add("Line" + indLine_.ToString());
+                foreach (string item in selection)
+                    line.Nodes.Add(item);
+                ++indLine_;
+            }
+            if (nSelection == 1)
+                treeSelection.Nodes.Add(selection[0]);
+        }
+
+        private void buttonEditSelection_Click(object sender, EventArgs e)
+        {
+            // If not a line was selected
+            if (treeSelection.SelectedNode == null || treeSelection.SelectedNode.Nodes.Count == 0)
+                return;
+            if (isEditSelection)
+            {
+                TreeNode line = treeSelection.Nodes[iSelectedSet_];
+                line.Nodes.Clear();
+                var selection = modelRenderer_.getSelection();
+                foreach (string item in selection)
+                    line.Nodes.Add(item);
+                if (selection.Count == 0) // If the resulting set is empty
+                    line.Remove();
+                iSelectedSet_ = -1;
+            }
+            else
+            { 
+                iSelectedSet_ = treeSelection.SelectedNode.Index;
+            }
+            // Invert the states of the controls
+            buttonAddSelection.Enabled = isEditSelection;
+            buttonRemoveSelection.Enabled = isEditSelection;
+            isEditSelection = !isEditSelection;
+        }
+
+        private void buttonRemoveSelection_Click(object sender, EventArgs e)
+        {
+            if (treeSelection.Nodes.Count == 0 || treeSelection.SelectedNode == null)
+                return;
+            int iSelected = treeSelection.SelectedNode.Index;
+            // If a child node is selected
+            if (treeSelection.SelectedNode.Parent != null)
+                return;
+            // Line
+            if (treeSelection.SelectedNode.Nodes.Count != 0)
+            {
+                foreach (TreeNode node in treeSelection.SelectedNode.Nodes)
+                    removeNodeFromTree(node);
+            }
+            // Single node
+            else 
+            {
+                removeNodeFromTree(treeSelection.SelectedNode);
+            }
+            treeSelection.Nodes.RemoveAt(iSelected);
+            modelRenderer_.draw();
+        }
+
+        private void removeNodeFromTree(TreeNode node)
+        {
+            string selectedNode = node.Text;
+            string[] selectionInfo = selectedNode.Split(selectionDelimiter_);
+            modelRenderer_.removeSeletion(selectionInfo[0], selectionInfo[1]);
+        }
+
+        private void glWindow_Load(object sender, EventArgs e)
+        {
+            glWindow.MakeCurrent();
+            modelRenderer_.setControl(glWindow);
+            // -- Debug only --
+            testRender();
+            // ----------------
+        }
+
+        private void setEnabled()
+        {
+            bool flag = project.isProjectOpened();
+            buttonAddSelection.Enabled = flag;
+            buttonRemoveSelection.Enabled = flag;
+            buttonEditSelection.Enabled = flag;
+        }
+
+        private void glWindow_Paint(object sender, PaintEventArgs e)
+        {
+            modelRenderer_.draw();
+        }
+
+        private void glWindow_MouseWheel(object sender, System.Windows.Forms.MouseEventArgs e)
+        {
+            float scale = 1.0f + MouseWeights.scaling * e.Delta;
+            modelRenderer_.setScale(scale);
+            modelRenderer_.draw();
+        }
+
+        private void glWindow_MouseDown(object sender, System.Windows.Forms.MouseEventArgs e)
+        {
+            var keyboard = Keyboard.GetState();
+            switch (e.Button)
+            {
+                case MouseButtons.Middle:
+                    lastMousePosition_[0] = e.X;
+                    lastMousePosition_[1] = e.Y;
+                    if (keyboard.IsKeyDown(Key.ControlLeft))
+                        isTranslation_ = true;
+                    else
+                        isRotation_ = true;
+                    break;
+                case MouseButtons.Left:
+                    bool isNewSelection = true;
+                    if (keyboard.IsKeyDown(Key.ShiftLeft) || isEditSelection)
+                        isNewSelection = false;
+                    modelRenderer_.select(e.X, e.Y, isNewSelection);
+                    modelRenderer_.draw();
+                    break;
+            }
+        }
+
+        private void glWindow_MouseMove(object sender, System.Windows.Forms.MouseEventArgs e)
+        {
+            var keyboard = Keyboard.GetState();
+            var mouse = Mouse.GetState();
+            if (isTranslation_)
+            {
+                float dX = (e.X - lastMousePosition_[0]) * MouseWeights.translation; 
+                float dY = (e.Y - lastMousePosition_[1]) * MouseWeights.translation;
+                modelRenderer_.setTranslation(dX, -dY);
+                modelRenderer_.draw();
+                lastMousePosition_[0] = e.X;
+                lastMousePosition_[1] = e.Y;
+            }
+            if (isRotation_)
+            {
+                float dRotX = (e.Y - lastMousePosition_[1]);
+                float dRotY = (e.X - lastMousePosition_[0]);
+                dRotX = MathHelper.DegreesToRadians(dRotX) * MouseWeights.rotation;
+                dRotY = MathHelper.DegreesToRadians(dRotY) * MouseWeights.rotation;
+                modelRenderer_.setRotationXY(dRotX, dRotY);
+                modelRenderer_.draw();
+                lastMousePosition_[0] = e.X;
+                lastMousePosition_[1] = e.Y;
+            }
+        }
+        private void glWindow_MouseUp(object sender, System.Windows.Forms.MouseEventArgs e)
+        {
+            var keyboard = Keyboard.GetState();
+            if (e.Button == MouseButtons.Middle) {
+                isTranslation_ = false;
+                isRotation_ = false;
+            }
+            if (keyboard.IsKeyUp(Key.ControlLeft)) 
+                isTranslation_ = false;
+        }
+
+        private void testRender()
+        {
+            //string path = Path.GetFullPath(@"..\..\..\examples\Plate.lms");
+            //string path = Path.GetFullPath(@"..\..\..\examples\Rib.lms");
+            //string path = Path.GetFullPath(@"..\..\..\examples\Airplane.lms");
+            string path = Path.GetFullPath(@"..\..\..\examples\Yak130.lms");
+            project = new LMSProject(path);
+            modelRenderer_.setGeometry(project.geometry_);
+            modelRenderer_.setView(LMSModel.Views.UP);
+            setEnabled();
+        }
+
+        private void glWindow_KeyDown(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyData)
+            {
+                case Keys.Control | Keys.F:
+                    modelRenderer_.setView(LMSModel.Views.UP);
+                    modelRenderer_.draw();
+                    break;
+                case Keys.Escape:
+                    modelRenderer_.clearSelection();
+                    modelRenderer_.draw();
+                    break;
+            }
+        }
+        private void glWindow_Resize(object sender, EventArgs e)
+        {
+            GL.Viewport(0, 0, glWindow.Width, glWindow.Height);
+        }
+
+        static class MouseWeights
+        {
+            public const float scaling = 0.001f;
+            public const float translation = 1.0f;
+            public const float rotation = 1.0f;
+        }
+    }
+}
